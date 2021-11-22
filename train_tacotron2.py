fd8cedf21f2e8daef6ee4c0c4bce7e0b7cd69c50
SunLu Z <40049927+zsl24@users.noreply.github.com>
2021-11-22 17:25:09 +0800

    Update train_tacotron2.py

--- train_tacotron2.py
+++ train_tacotron2.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 from tensorboardX import SummaryWriter
-# from torch import nn
 from tqdm import tqdm
 
 import config
@@ -23,7 +22,7 @@ def train_net(args):
     start_epoch = 0
     best_loss = float('inf')
     writer = SummaryWriter()
-    epochs_since_improvement = 0
+    steps_since_improvement = 0
 
     # Initialize / load checkpoint
     if checkpoint is None:
@@ -38,25 +37,23 @@ def train_net(args):
 
     else:
         print(checkpoint)
-        load_mode = 'p'
+        load_mode = 'dict'
         if load_mode == 'dict':
             checkpoint = torch.load(checkpoint)
             model = checkpoint['model']
-            epochs_since_improvement = checkpoint['epochs_since_improvement']
+            step = checkpoint['step']
+            steps_since_improvement = checkpoint['steps_since_improvement']
             model = checkpoint['model']
             optimizer = checkpoint['optimizer']
+            optimizer.lr = 1e-4
         else:
             checkpoint = torch.load(checkpoint)
             start_epoch = 0
             model = Tacotron2(config)
             model.load_state_dict(checkpoint)
+            optimizer = Tacotron2Optimizer(
+                torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2, betas=(0.9, 0.999), eps=1e-6))
         print(model)
-        # epochs_since_improvement = checkpoint['epochs_since_improvement']
-        # model = checkpoint['model']
-        # optimizer = checkpoint['optimizer']
-        optimizer = Tacotron2Optimizer(
-            torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2, betas=(0.9, 0.999), eps=1e-6))
-
     logger = get_logger()
 
     # Move to GPU, if available
@@ -75,110 +72,82 @@ def train_net(args):
     valid_dataset = TextMelLoader(config.validation_files, config)
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, collate_fn=collate_fn,
                                                pin_memory=True, shuffle=False, num_workers=args.num_workers)
-
+    
     # Epochs
     for epoch in range(start_epoch, args.epochs):
         # One epoch's training
-        
-        train_loss = train(train_loader=train_loader,
-                           model=model,
-                           optimizer=optimizer,
-                           criterion=criterion,
-                           epoch=epoch,
-                           logger=logger)
-        writer.add_scalar('model/train_loss', train_loss, epoch)
-
-        lr = optimizer.lr
-        print('\nLearning rate: {}'.format(lr))
-        writer.add_scalar('model/learning_rate', lr, epoch)
-        step_num = optimizer.step_num
-        print('Step num: {}\n'.format(step_num))
-        
-
-        # One epoch's validation
-        valid_loss = valid(valid_loader=valid_loader,
-                           model=model,
-                           criterion=criterion,
-                           logger=logger)
-        writer.add_scalar('model/valid_loss', valid_loss, epoch)
-
-        # Check if there was an improvement
-        is_best = valid_loss < best_loss
-        best_loss = min(valid_loss, best_loss)
-        if not is_best:
-            epochs_since_improvement += 1
-            print("\nEpochs since last improvement: %d\n" % (epochs_since_improvement,))
-        else:
-            epochs_since_improvement = 0
-        
-        # Save checkpoint
-        save_checkpoint(epoch, epochs_since_improvement, model, optimizer, best_loss, is_best)
-
-        # alignments
-        img_align = test(model, optimizer.step_num, valid_loss)
-        writer.add_image('model/alignment', img_align, epoch, dataformats='HWC')
-
-
-def train(train_loader, model, optimizer, criterion, epoch, logger):
-    model.train()  # train mode (dropout and batchnorm is used)
-
-    losses = AverageMeter()
-
-    # Batches
-    
-    for i, batch in enumerate(train_loader):
-        model.zero_grad()
-
-        x, y = model.parse_batch(batch)
-
-        # Forward prop.
-        y_pred = model(x)
-
-        loss = criterion(y_pred, y)
-
-        # Back prop.
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Update weights
-        optimizer.step()
-
-        # Keep track of metrics
-        losses.update(loss.item())
-        torch.cuda.empty_cache()
-        # Print status
-        if i % args.print_freq == 0:
-            logger.info('Epoch: [{0}][{1}/{2}]\t'
-                        'Loss {loss.val:.4f} ({loss.avg:.4f})'.format(epoch, i, len(train_loader), loss=losses))
-
-    return losses.avg
-
-
-def valid(valid_loader, model, criterion, logger):
-    model.eval()
-
-    losses = AverageMeter()
-
-    # Batches
-    with torch.no_grad():
-        for batch in tqdm(valid_loader):
-            #model.zero_grad()
+        losses = AverageMeter()
+        for i, batch in enumerate(train_loader):
+            model.train()
+            model.zero_grad()
             x, y = model.parse_batch(batch)
 
             # Forward prop.
             y_pred = model(x)
 
+            # loss
             loss = criterion(y_pred, y)
 
+            # Back prop.
+            optimizer.zero_grad()
+            loss.backward()
+
+            # Update weights
+            optimizer.step()
+
             # Keep track of metrics
-            print(loss.item())
             losses.update(loss.item())
+            torch.cuda.empty_cache()
+            
+            # 记录以供tensorboard查看
+            writer.add_scalar('model/train_loss', losses.val, optimizer.step_num)
 
-        # Print status
-        logger.info('\nValidation Loss {loss.val:.4f} ({loss.avg:.4f})\n'.format(loss=losses))
+            # Print status
+            if i % args.print_freq == 0:
+                logger.info('Epoch: [{0}][{1}/{2}]\t'
+                            'Loss {loss.val:.4f} ({loss.avg:.4f})'.format(epoch, i, len(train_loader), loss=losses))
+                
+            # validation
+            if i % config.validation_steps == 0:
+                valid_losses = AverageMeter()
+                model.eval()
+                lr = optimizer.lr
+                step_num = optimizer.step_num
+                print('\nLearning rate: {}'.format(lr))
+                writer.add_scalar('model/learning_rate', lr, step_num)
+                
+                print('Step num: {}\n'.format(step_num))
+        
+                with torch.no_grad(): #试试这里，试完记得还原train的代码
+                    for batch in valid_loader:
+                        #model.zero_grad()
+                        x, y = model.parse_batch(batch)
+                        # Forward prop.
+                        y_pred = model(x)
+                        loss = criterion(y_pred, y)
+                        # Keep track of metrics
+                        valid_losses.update(loss.item())
+                valid_loss = valid_losses.avg
+                writer.add_scalar('model/valid_loss', valid_loss, step_num)
 
-    return losses.avg
+                logger.info('Epoch: [{0}][{1}/{2}]\t'
+                            'Validation Loss {loss:.4f}'.format(epoch, i, len(train_loader), loss=valid_loss))
 
+                # Check if there was an improvement
+                is_best = valid_loss < best_loss
+                best_loss = min(valid_loss, best_loss)
+                if not is_best:
+                    steps_since_improvement += config.validation_steps
+                    print("\nSteps since last improvement: %d\n" % (steps_since_improvement,))
+                else:
+                    steps_since_improvement = 0
+
+                # Save checkpoint                              
+                save_checkpoint(epoch, step_num, steps_since_improvement, model, optimizer, best_loss, is_best)
+                
+                # Show alignments
+                img_align = test(model, step_num, valid_loss)
+                writer.add_image('model/alignment', img_align, step_num, dataformats='HWC')
 
 def main():
     global args
