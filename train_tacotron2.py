69cb253a1ec327378bb702c8bd67c995549b8b4d
SunLu Z <40049927+zsl24@users.noreply.github.com>
2022-01-29 16:32:50 +0800

    Update train_tacotron2.py

--- train_tacotron2.py
+++ train_tacotron2.py
@@ -2,18 +2,16 @@ import numpy as np
 import torch
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
-
+import argparse
 import config
 from data_gen import TextMelLoader, TextMelCollate
 from taco2models.loss_function import Tacotron2Loss
 from taco2models.models import Tacotron2
 from taco2models.optimizer import Tacotron2Optimizer
-from utils_1 import parse_args, save_checkpoint, AverageMeter, get_logger, test
+from utils_1 import save_checkpoint, AverageMeter, get_logger, test
 
 
 import os
-
-os.environ['CUDA_VISIBLE_DEVICES'] ='7'
 
 def train_net(args):
     torch.manual_seed(7)
@@ -26,6 +24,7 @@ def train_net(args):
 
     # Initialize / load checkpoint
     if checkpoint is None:
+        print('Training from scratch ...')
         # model
         model = Tacotron2(config)
         print(model)
@@ -34,29 +33,31 @@ def train_net(args):
         # optimizer
         optimizer = Tacotron2Optimizer(
             torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2, betas=(0.9, 0.999), eps=1e-6))
-
     else:
-        print(checkpoint)
-        load_mode = 'dict'
+        print('Loading model:{}'.format(checkpoint))
+        load_mode = args.load_type
         if load_mode == 'dict':
             checkpoint = torch.load(checkpoint)
             model = checkpoint['model']
-            step = checkpoint['step']
+            start_epoch = checkpoint['epoch'] + 1
+            step = checkpoint['step'] + 1
             steps_since_improvement = checkpoint['steps_since_improvement']
-            model = checkpoint['model']
             optimizer = checkpoint['optimizer']
-            optimizer.lr = 1e-4
+            model = checkpoint['model']
+            best_loss = checkpoint['loss']
+            if best_loss < 0.4:
+                # 为了防止loss由于best_loss太低导致 loss一直无法下降到best_loss而导致best_checkpoint存不下来
+                best_loss = 0.4
         else:
             checkpoint = torch.load(checkpoint)
-            start_epoch = 0
             model = Tacotron2(config)
             model.load_state_dict(checkpoint)
             optimizer = Tacotron2Optimizer(
                 torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2, betas=(0.9, 0.999), eps=1e-6))
         print(model)
     logger = get_logger()
+    print(f'learning rate is',optimizer.lr)
 
-    # Move to GPU, if available
     model = model.to(config.device)
 
     criterion = Tacotron2Loss()
@@ -64,15 +65,22 @@ def train_net(args):
     collate_fn = TextMelCollate(config.n_frames_per_step)
 
     # Custom dataloaders
-    # 创建一个能够访问(text,mel) pair的具体文件的接口，text是以一维数组的形式，mel是以二维数组的形式
-    train_dataset = TextMelLoader(config.training_files, config) 
-
+    if args.dataset == 'biaobei':
+        training_files = args.dataset + '_filelist/' + args.dataset + '_audio_text_train_filelist.txt'
+        validation_files = args.dataset + '_filelist/' + args.dataset + '_audio_text_valid_filelist.txt'
+    else:
+        training_files = args.dataset + '_filelist/' + args.dataset + '_audio_text_train_filelist.json'
+        validation_files = args.dataset + '_filelist/' + args.dataset + '_audio_text_valid_filelist.json'
+    
+    train_dataset = TextMelLoader(training_files, config, dataset=args.dataset) 
+    print('batch size is ', args.batch_size)
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=collate_fn,
                                                pin_memory=True, shuffle=True, num_workers=args.num_workers)
-    valid_dataset = TextMelLoader(config.validation_files, config)
+    print(f'loaded dataset from {training_files}')
+    valid_dataset = TextMelLoader(validation_files, config, dataset=args.dataset)
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=args.batch_size, collate_fn=collate_fn,
                                                pin_memory=True, shuffle=False, num_workers=args.num_workers)
-    
+    print(f'loaded dataset from {validation_files}')
     # Epochs
     for epoch in range(start_epoch, args.epochs):
         # One epoch's training
@@ -99,28 +107,24 @@ def train_net(args):
             losses.update(loss.item())
             torch.cuda.empty_cache()
             
-            # 记录以供tensorboard查看
             writer.add_scalar('model/train_loss', losses.val, optimizer.step_num)
 
             # Print status
             if i % args.print_freq == 0:
                 logger.info('Epoch: [{0}][{1}/{2}]\t'
                             'Loss {loss.val:.4f} ({loss.avg:.4f})'.format(epoch, i, len(train_loader), loss=losses))
-                
             # validation
-            if i % config.validation_steps == 0:
+            if i % config.validation_steps == 0 and i != 0:
                 valid_losses = AverageMeter()
                 model.eval()
                 lr = optimizer.lr
                 step_num = optimizer.step_num
                 print('\nLearning rate: {}'.format(lr))
                 writer.add_scalar('model/learning_rate', lr, step_num)
-                
                 print('Step num: {}\n'.format(step_num))
-        
-                with torch.no_grad(): #试试这里，试完记得还原train的代码
+                with torch.no_grad():
                     for batch in valid_loader:
-                        #model.zero_grad()
+                        model.zero_grad()
                         x, y = model.parse_batch(batch)
                         # Forward prop.
                         y_pred = model(x)
@@ -129,7 +133,6 @@ def train_net(args):
                         valid_losses.update(loss.item())
                 valid_loss = valid_losses.avg
                 writer.add_scalar('model/valid_loss', valid_loss, step_num)
-
                 logger.info('Epoch: [{0}][{1}/{2}]\t'
                             'Validation Loss {loss:.4f}'.format(epoch, i, len(train_loader), loss=valid_loss))
 
@@ -142,18 +145,38 @@ def train_net(args):
                 else:
                     steps_since_improvement = 0
 
-                # Save checkpoint                              
-                save_checkpoint(epoch, step_num, steps_since_improvement, model, optimizer, best_loss, is_best)
-                
-                # Show alignments
+                # saving checkpoint and update the best checkpoint                             
+                save_checkpoint(epoch, step_num, steps_since_improvement, model, optimizer, best_loss, is_best, dataset=args.dataset,trial_type=args.trial_type)
+                # drawing alignment
                 img_align = test(model, step_num, valid_loss)
                 writer.add_image('model/alignment', img_align, step_num, dataformats='HWC')
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tacotron2')
+    parser.add_argument('--epochs', default=10000, type=int)
+    parser.add_argument('--max_norm', default=1, type=float, help='Gradient norm threshold to clip')
+    # trial type
+    parser.add_argument('--trial_type', type=str, default='new', help='new vaocal dict or old vaocal dict')
+    parser.add_argument('--load_type', type=str, default='dict', help='method to load model')
+    # dataset 
+    parser.add_argument('--dataset', type=str, default='aixia', help='name of dataset')
+    # minibatch
+    parser.add_argument('--batch_size', default=16, type=int)
+    parser.add_argument('--num-workers', default=4, type=int, help='Number of workers to generate minibatch')
+    # logging
+    parser.add_argument('--print_freq', default=10, type=int, help='Frequency of printing training information')
+    # optimizer
+    parser.add_argument('--lr', default=1e-3, type=float, help='Init learning rate')
+    parser.add_argument('--l2', default=1e-6, type=float, help='weight decay (L2)')
+    parser.add_argument('--checkpoint', type=str, default='biaobei_checkpoints/biaobei.tar', help='checkpoint')
+    args = parser.parse_args()
+    return args
 
 def main():
     global args
     args = parse_args()
     train_net(args)
 
-
 if __name__ == '__main__':
     main()
+
