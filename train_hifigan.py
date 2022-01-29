d0e38ac72db3f537fbdb277e1359703d6fd17a36
SunLu Z <40049927+zsl24@users.noreply.github.com>
2022-01-29 16:35:55 +0800

    Update train_hifigan.py

--- train_hifigan.py
+++ train_hifigan.py
@@ -2,6 +2,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import itertools
 import os
+#os.environ["CUDA_VISIBLE_DEVICES"]= "0,1,2,3,4"
 import time
 import argparse
 import json
@@ -13,10 +14,11 @@ import torch.multiprocessing as mp
 from torch.distributed import init_process_group
 from torch.nn.parallel import DistributedDataParallel
 from env import AttrDict, build_env
-from meldataset import MelDataset, mel_spectrogram, get_dataset_filelist
+from meldataset import MelDataset, mel_spectrogram, get_dataset_filelist_fromjson
 from models import Generator, MultiPeriodDiscriminator, MultiScaleDiscriminator, feature_loss, generator_loss,\
     discriminator_loss
 from utils import plot_spectrogram, scan_checkpoint, load_checkpoint, save_checkpoint
+
 
 torch.backends.cudnn.benchmark = True
 
@@ -32,28 +34,37 @@ def train(rank, a, h):
     generator = Generator(h).to(device)
     mpd = MultiPeriodDiscriminator().to(device)
     msd = MultiScaleDiscriminator().to(device)
+    best_loss = 100
 
     if rank == 0:
         print(generator)
         os.makedirs(a.checkpoint_path, exist_ok=True)
         print("checkpoints directory : ", a.checkpoint_path)
 
-    if os.path.isdir(a.checkpoint_path):
-        cp_g = scan_checkpoint(a.checkpoint_path, 'g_')
-        cp_do = scan_checkpoint(a.checkpoint_path, 'do_')
 
+    cp_g = a.generator
+    cp_do = a.discriminator
     steps = 0
     if cp_g is None or cp_do is None:
         state_dict_do = None
         last_epoch = -1
     else:
         state_dict_g = load_checkpoint(cp_g, device)
+        print('loading generator from:' + cp_g)
         state_dict_do = load_checkpoint(cp_do, device)
+        print('loading discriminator from:' + cp_do)
         generator.load_state_dict(state_dict_g['generator'])
         mpd.load_state_dict(state_dict_do['mpd'])
         msd.load_state_dict(state_dict_do['msd'])
         steps = state_dict_do['steps'] + 1
         last_epoch = state_dict_do['epoch']
+        if 'loss' in state_dict_g:
+            best_loss = state_dict_g['loss']
+            if best_loss < 0.3:
+                best_loss = 0.3
+        else:
+            best_loss = 0.5
+        print('current loss is',best_loss)
 
     if h.num_gpus > 1:
         generator = DistributedDataParallel(generator, device_ids=[rank]).to(device)
@@ -71,8 +82,11 @@ def train(rank, a, h):
     scheduler_g = torch.optim.lr_scheduler.ExponentialLR(optim_g, gamma=h.lr_decay, last_epoch=last_epoch)
     scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=h.lr_decay, last_epoch=last_epoch)
 
-    training_filelist, validation_filelist = get_dataset_filelist(a)
-
+    training_filelist, validation_filelist = get_dataset_filelist_fromjson(a)
+    # trainset = (mel, audio, filename, mel_loss)
+    # fine-tuning = True
+    # base_mels_path是Tacotron2版本的mels的文件夹路径
+    print('fine_tuning',a.fine_tuning)
     trainset = MelDataset(training_filelist, h.segment_size, h.n_fft, h.num_mels,
                           h.hop_size, h.win_size, h.sampling_rate, h.fmin, h.fmax, n_cache_reuse=0,
                           shuffle=False if h.num_gpus > 1 else True, fmax_loss=h.fmax_for_loss, device=device,
@@ -96,8 +110,8 @@ def train(rank, a, h):
                                        batch_size=1,
                                        pin_memory=True,
                                        drop_last=True)
-
-        sw = SummaryWriter(os.path.join(a.checkpoint_path, 'logs'))
+        localtime = time.localtime(time.time())
+        sw = SummaryWriter(os.path.join(a.checkpoint_path, f'{localtime.tm_year}-{localtime.tm_mon}-{localtime.tm_mday}'))
 
     generator.train()
     mpd.train()
@@ -113,6 +127,9 @@ def train(rank, a, h):
         for i, batch in enumerate(train_loader):
             if rank == 0:
                 start_b = time.time()
+            # x - input mel
+            # y - target audio
+
             x, y, _, y_mel = batch
             x = torch.autograd.Variable(x.to(device, non_blocking=True))
             y = torch.autograd.Variable(y.to(device, non_blocking=True))
@@ -156,33 +173,6 @@ def train(rank, a, h):
             optim_g.step()
 
             if rank == 0:
-                # STDOUT logging
-                if steps % a.stdout_interval == 0:
-                    with torch.no_grad():
-                        mel_error = F.l1_loss(y_mel, y_g_hat_mel).item()
-
-                    print('Steps : {:d}, Gen Loss Total : {:4.3f}, Mel-Spec. Error : {:4.3f}, s/b : {:4.3f}'.
-                          format(steps, loss_gen_all, mel_error, time.time() - start_b))
-
-                # checkpointing
-                if steps % a.checkpoint_interval == 0 and steps != 0:
-                    checkpoint_path = "{}/g_{:08d}".format(a.checkpoint_path, steps)
-                    save_checkpoint(checkpoint_path,
-                                    {'generator': (generator.module if h.num_gpus > 1 else generator).state_dict()})
-                    checkpoint_path = "{}/do_{:08d}".format(a.checkpoint_path, steps)
-                    save_checkpoint(checkpoint_path, 
-                                    {'mpd': (mpd.module if h.num_gpus > 1
-                                                         else mpd).state_dict(),
-                                     'msd': (msd.module if h.num_gpus > 1
-                                                         else msd).state_dict(),
-                                     'optim_g': optim_g.state_dict(), 'optim_d': optim_d.state_dict(), 'steps': steps,
-                                     'epoch': epoch})
-
-                # Tensorboard summary logging
-                if steps % a.summary_interval == 0:
-                    sw.add_scalar("training/gen_loss_total", loss_gen_all, steps)
-                    sw.add_scalar("training/mel_spec_error", mel_error, steps)
-
                 # Validation
                 if steps % a.validation_interval == 0:  # and steps != 0:
                     generator.eval()
@@ -214,6 +204,42 @@ def train(rank, a, h):
                         sw.add_scalar("validation/mel_spec_error", val_err, steps)
 
                     generator.train()
+                # STDOUT logging
+                if steps % a.stdout_interval == 0:
+                    with torch.no_grad():
+                        mel_error = F.l1_loss(y_mel, y_g_hat_mel).item()
+
+                    print('Steps : {:d}, Gen Loss Total : {:4.3f}, Mel-Spec. Error : {:4.3f}, s/b : {:4.3f}'.
+                          format(steps, loss_gen_all, mel_error, time.time() - start_b))
+
+                # checkpointing
+                if steps % a.checkpoint_interval == 0 and steps != 0:
+                    checkpoint_path = "{}/generator_{}".format(a.checkpoint_path,a.version)
+                    save_checkpoint(checkpoint_path,
+                                    {'generator': (generator.module if h.num_gpus > 1 else generator).state_dict(),
+                                    'loss':val_err})
+                    if val_err < best_loss:
+                        print(f'saving best ckp with loss {val_err}')
+                        checkpoint_path = "{}/BEST_generator_{}_{:.2f}".format(a.checkpoint_path,a.version,val_err)
+                        best_loss = val_err
+                        save_checkpoint(checkpoint_path,
+                                        {'generator': (generator.module if h.num_gpus > 1 else generator).state_dict(),
+                                        'loss':val_err})                  
+                    checkpoint_path = "{}/discriminator".format(a.checkpoint_path)
+                    save_checkpoint(checkpoint_path, 
+                                    {'mpd': (mpd.module if h.num_gpus > 1
+                                                         else mpd).state_dict(),
+                                     'msd': (msd.module if h.num_gpus > 1
+                                                         else msd).state_dict(),
+                                     'optim_g': optim_g.state_dict(), 'optim_d': optim_d.state_dict(), 'steps': steps,
+                                     'epoch': epoch})
+
+                # Tensorboard summary logging
+                if steps % a.summary_interval == 0:
+                    sw.add_scalar("training/gen_loss_total", loss_gen_all, steps)
+                    sw.add_scalar("training/mel_spec_error", mel_error, steps)
+
+
 
             steps += 1
 
@@ -230,18 +256,23 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--group_name', default=None)
-    parser.add_argument('--input_wavs_dir', default='LJSpeech-1.1/wavs')
-    parser.add_argument('--input_mels_dir', default='ft_dataset')
-    parser.add_argument('--input_training_file', default='LJSpeech-1.1/training.txt')
-    parser.add_argument('--input_validation_file', default='LJSpeech-1.1/validation.txt')
-    parser.add_argument('--checkpoint_path', default='cp_hifigan')
-    parser.add_argument('--config', default='')
+    parser.add_argument('--input_wavs_dir', default='/home/yanan/zengsunlu/hifigan_ft/aixia/wav', type=str)
+    parser.add_argument('--input_mels_dir', default='/home/yanan/zengsunlu/hifigan_ft/aixia/mel_tf', type=str)
+    parser.add_argument('--input_training_file', default='hifigan_filelist/training.json', type=str)
+    parser.add_argument('--input_validation_file', default='hifigan_filelist/validation.json', type=str)
+    parser.add_argument('--checkpoint_path', default='ckp_hifigan', type=str)
+    parser.add_argument('--generator', default=None, type=str)
+    parser.add_argument('--config', default='ft_model/config.json', type=str)
+    parser.add_argument('--discriminator', default=None, type=str)
     parser.add_argument('--training_epochs', default=3100, type=int)
     parser.add_argument('--stdout_interval', default=5, type=int)
-    parser.add_argument('--checkpoint_interval', default=5000, type=int)
-    parser.add_argument('--summary_interval', default=100, type=int)
-    parser.add_argument('--validation_interval', default=1000, type=int)
-    parser.add_argument('--fine_tuning', default=False, type=bool)
+    parser.add_argument('--checkpoint_interval', default=100, type=int)
+    parser.add_argument('--summary_interval', default=10, type=int)
+    parser.add_argument('--validation_interval', default=100, type=int)
+    parser.add_argument('--fine_tuning', default=True, type=bool)
+    parser.add_argument('--version', default='v1', type=str)
+    parser.add_argument('--n_gpu', default=1, type=int)
+    parser.add_argument('--batch_size', default=16, type=int)
 
     a = parser.parse_args()
 
@@ -251,11 +282,12 @@ def main():
     json_config = json.loads(data)
     h = AttrDict(json_config)
     build_env(a.config, 'config.json', a.checkpoint_path)
+    h.batch_size = a.batch_size
 
     torch.manual_seed(h.seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed(h.seed)
-        h.num_gpus = torch.cuda.device_count()
+        h.num_gpus = a.n_gpu
         h.batch_size = int(h.batch_size / h.num_gpus)
         print('Batch size per GPU :', h.batch_size)
     else:
